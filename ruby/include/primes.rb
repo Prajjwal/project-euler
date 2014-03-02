852a1c4f5a4b9a3322a153710bb741dc5f35f6cc
Prajjwal Singh <sin@prajjwal.com>
2014-03-02 17:10:39 +0530

    Improved ruby prime finder

--- ruby/include/primes.rb
+++ ruby/include/primes.rb
@@ -4,13 +4,10 @@
 # https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 def primes_upto(n)
   sieve = [].fill(true, (2..n))
-  primes = []
 
   2.upto Math.sqrt(n) do |i|
     (i * i).step(n, i) { |j| sieve[j] = false } if sieve[i]
   end
 
-  sieve.each_with_index { |b, i| primes.push(i) if b }
-
-  primes
+  sieve.each_index.select { |i| i if sieve[i] }
 end
