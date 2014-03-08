edbc31a67959ffb092927aeb2a3949fe7cf9f991
Prajjwal Singh <sin@prajjwal.com>
2014-03-08 17:35:21 +0530

    improve ruby soln to p13

--- ruby/p13.rb
+++ ruby/p13.rb
@@ -1,3 +1,1 @@
-File.open "../input/p13.txt", "r" do |f|
-  puts f.readlines.map { |n| n.to_i }.reduce(:+).to_s[0, 10]
-end
+puts IO.readlines("../input/p13.txt").map { |n| n.to_i }.reduce(:+).to_s[0, 10]
