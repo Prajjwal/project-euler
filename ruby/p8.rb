0277aaec5c14778d3005f670cfdff3054bca853a
Prajjwal Singh <sin@prajjwal.com>
2014-03-11 21:17:07 +0530

    Improve problem 8 solution in ruby

--- ruby/p8.rb
+++ ruby/p8.rb
@@ -2,10 +2,7 @@ num = File.open("../input/p8.txt", "r") { |f| f.readline }
 largest_p = 1
 
 0.upto 996 do |i|
-  p = 1
-
-  num[i, 5].each_byte { |b| p *= b - 48 }
-
+  p = num[i, 5].each_char.map(&:to_i).reduce(:*)
   largest_p = p if p > largest_p
 end
 
