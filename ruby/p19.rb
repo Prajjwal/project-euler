5cf6fe3a92c5e79df516c443641f862a9a8c0ec6
Prajjwal Singh <sin@prajjwal.com>
2014-04-14 22:27:34 +0530

    Use range instead of upto()

--- ruby/p19.rb
+++ ruby/p19.rb
@@ -3,4 +3,4 @@ require 'date'
 s_date = Date.new(1901, 1, 1)
 e_date = Date.new(2000, 12, 31)
 
-puts s_date.upto(e_date).count { |d| d.sunday? && d.day == 1 }
+puts (s_date..e_date).count { |d| d.sunday? && d.day == 1 }
